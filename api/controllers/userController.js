2fef527a56d6c4294f4149c0c7352f37e2e41124
MohitGupta <krishgupta1630@gmail.com>
2023-11-23 22:32:04 +0530

    complete admin access api routes

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -49,7 +49,65 @@ export const updateCurrentUserProfile = async (req, res, next) => {
 
 // ============================== Admin Controller =======================================
 
-export const getAllUsers = async (req, res, next) => {};
-export const getUserById = async (req, res, next) => {};
-export const updateUserById = async (req, res, next) => {};
-export const deleteUserById = async (req, res, next) => {};
+export const getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({}).select("-password");
+    res.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserById = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id).select("-password");
+    if (!user) {
+      return next(errorHandler(404, "User not found !"));
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateUserById = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (user) {
+      user.username = req.body.username || user.username;
+      user.email = req.body.email || user.email;
+      user.isAdmin = Boolean(req.body.isAdmin);
+
+      const updatedUser = await user.save();
+
+      res.status(200).json({
+        _id: updatedUser._id,
+        username: updatedUser.username,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+      });
+    } else {
+      next(errorHandler(404, "User not found !"));
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUserById = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (user.isAdmin) {
+      return next(errorHandler(400, "You cannot delete Admin !"));
+    }
+
+    await User.deleteOne({ _id: user._id });
+
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -8,7 +8,10 @@ import {
   updateUserById,
   deleteUserById,
 } from "../controllers/userController.js";
-import { authenticateUser } from "../middlewares/authMiddleware.js";
+import {
+  authenticateUser,
+  authorizeAdmin,
+} from "../middlewares/authMiddleware.js";
 
 // ========================== User Routes ===========================
 
@@ -29,21 +32,21 @@ router
 // @desc    Get all users list
 // method   GET
 // url      /api/users
-router.get("/", getAllUsers);
+router.get("/", authenticateUser, authorizeAdmin, getAllUsers);
 
 // @desc    Get user by id
 // method   GET
 // url      /api/users/:id
-router.get("/:id", getUserById);
+router.get("/:id", authenticateUser, authorizeAdmin, getUserById);
 
 // @desc    Update user by id
 // method   PUT
 // url      /api/users/:id
-router.put("/:id", updateUserById);
+router.put("/:id", authenticateUser, authorizeAdmin, updateUserById);
 
 // @desc    Delete user by id
 // method   DELETE
 // url      /api/users/:id
-router.get("/:id", deleteUserById);
+router.delete("/:id", authenticateUser, authorizeAdmin, deleteUserById);
 
 export default router;
